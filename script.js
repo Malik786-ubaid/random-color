5be597dad9fead83ff2df1393e0fda0e5cd66010
Malik Ubaid Awan <malikubaidawan14@gmail.com>
2026-04-30 13:11:55 +0500

    Add color-changing background functionality

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const randomColor = function () {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+let intervalId = null;
+
+const changeBgColor = function () {
+  document.body.style.backgroundColor = randomColor();
+};
+
+const startChangingColor = function () {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+    changeBgColor();
+  }
+};
+
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
 
